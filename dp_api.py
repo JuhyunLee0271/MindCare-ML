4af0fb1082be49992977788e1071168af653b482
Ubuntu <ubuntu@ip-172-31-46-19.ap-northeast-2.compute.internal>
2022-05-21 06:01:29 +0000

    mod minor bug in dp_api

--- dp_api.py
+++ dp_api.py
@@ -1,4 +1,6 @@
 import pymysql
+
+# RDS CONFIG
 HOST = 'flask-db.cuqw33e66jfm.ap-northeast-2.rds.amazonaws.com'
 USER = 'admin'
 PW = 'capstoneml'
@@ -6,11 +8,6 @@ DB = 'recommendation'
 CHARSET = 'utf8'
 PORT = 3306
 
-
-"""
-db_api.py
-db에 접근하는 api들
-"""
 # RDS connection Method
 def connect_to_db():
     conn, cur = None, None
@@ -37,11 +34,17 @@ def disconnect_from_db(conn, cur):
 # musicId -> {'title': title, 'artist': artist}
 def lookup_music_info(musicId):
     conn, cur = connect_to_db()
-    
-    cur.execute("SELECT title, artist FROM MUSIC WHERE musicId = ?", [musicId])
+    query = "SELECT title, artist FROM MUSIC WHERE musicId = %s"
+    param = (musicId)
+    cur.execute(query, param)
     result = cur.fetchall()
     if not result:
         return ""
     
     disconnect_from_db(conn, cur)
     return {'title': result[0][0], 'artist': result[0][1]}
+
+if __name__ == "__main__":
+    conn, cur = connect_to_db()
+    print(lookup_music_info(174749))
+    disconnect_from_db(conn, cur)
