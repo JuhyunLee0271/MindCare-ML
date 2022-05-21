e4c5e918f969b93c04665f72232eb2caaff5c5d5
juhyunlee <wngus392@naver.com>
2022-05-21 14:54:03 +0900

    transfer to ec2 instances

--- dp_api.py
+++ dp_api.py
@@ -11,7 +11,7 @@ PORT = 3306
 db_api.py
 db에 접근하는 api들
 """
-
+# RDS connection Method
 def connect_to_db():
     conn, cur = None, None
     try:
@@ -36,3 +36,12 @@ def disconnect_from_db(conn, cur):
         
 # musicId -> {'title': title, 'artist': artist}
 def lookup_music_info(musicId):
+    conn, cur = connect_to_db()
+    
+    cur.execute("SELECT title, artist FROM MUSIC WHERE musicId = ?", [musicId])
+    result = cur.fetchall()
+    if not result:
+        return ""
+    
+    disconnect_from_db(conn, cur)
+    return {'title': result[0][0], 'artist': result[0][1]}
--- extractor.py
+++ extractor.py
@@ -19,23 +19,44 @@ METHODS
             {'keyword3': '쉑쉑버거'}
         ]
 """
+"""
+!pip install mxnet
+!pip install gluonnlp pandas tqdm
+!pip install sentencepiece
+!pip install transformers==3.0.2
+!pip install torch
+!pip install git+https://git@github.com/SKTBrain/KoBERT.git@master
+"""
 
 # requirements
 from krwordrank.word import summarize_with_keywords
 from krwordrank.hangle import normalize
 from konlpy.tag import Okt
 import kss
+import numpy as np
+"""
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+"""
 
 class extractor:
     def __init__(self):
         self.keyword_extractor = Keyword_Extractor()
-        self.kobert_model = KoBERT_model()
+        self.sentiment_extractor = Sentiment_Extractor()
     
     # extract emotion from content(diary)
     def extract_sentiment_from_diary(self, content):
-        emotion = "기쁨"
-        # emotion = self.kobert_model.run()
-        return emotion 
+        emotion = self.sentiment_extractor.run(content)
+        return emotion
     
     # extract keywords from content(diary)
     def extract_keyword_from_diary(self, content):
@@ -63,5 +84,115 @@ class Keyword_Extractor:
             return [tag for tag in self.okt.nouns(' '.join(keywords)) if tag not in self.stopwords][:3]
         except:
             return []
+
+class Sentiment_Extractor:
+    def __init__(self):
+        # Load CUDA and default bertmodel, vocabulary 
+        self.device = torch.device("cuda:0")
+        self.bertmodel, self.vocab = get_pytorch_kobert_model()
         
-class KoBERT_model:
+        self.emo_dict = {0: '중립', 1: '걱정', 2: '슬픔', 3: '분노', 4: '행복'}
+        
+        # Setting Hyper-parameters
+        self.max_len = 64
+        self.batch_size = 32
+        self.warmup_ratio = 0.1
+        self.num_epochs = 20
+        self.max_grad_norm = 1
+        self.log_interval = 100
+        self.learning_rate = 10e-5
+        
+        # Load pretrained model
+        self.model = torch.load('./model/trained_kobert.pt')
+        self.model.load_state_dict(torch.load('./model/model_state_dict.pt'))
+
+        # Load tokenizer
+        self.tokenizer = get_tokenizer()
+        self.tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+        
+        
+    # Define softmax Function
+    def new_softmax(self, a):
+        c = np.max(a)
+        exp_a = np.exp(a-c)
+        sum_exp_a = np.sum(exp_a)
+        y = (exp_a / sum_exp_a) * 100
+        return np.round(y, 3)
+    
+    # Return Prediction of Emotion for diary content 
+    def run(self, sentence):
+        data = [sentence, '0']
+        dataset_another = [data]
+        
+        another_test = BERTDataset(dataset_another, 0, 1, self.tok, self.max_len, True, False)
+        test_dataloader = torch.utils.data.DataLoader(
+                        another_test, batch_size=self.batch_size, num_workers=5)
+        
+        self.model.eval()
+        
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+            token_ids = token_ids.long().to(self.device)
+            segment_ids = segment_ids.long().to(self.device)
+            
+            valid_length = valid_length
+            label = label.long().to(self.device)
+            
+            out = self.model(token_ids, valid_length, segment_ids)
+            for i in out:
+                logits = i
+                logits = logits.detach().cpu().numpy()
+                probability = []
+                logits = np.round(self.new_softmax(logits), 3).tolist()
+                for logit in logits:
+                    probability.append(np.round(logit, 3))
+                emotion = self.emo_dict[np.argmax(logits)]
+            return emotion
+        
+"""
+Helper Class for KoBERT
+"""
+# Define Model for KoBERT Classifier
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=5,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+    
+# Input Dataset for KoBERT
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels)) 
